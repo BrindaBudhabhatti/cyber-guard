29e40b475ab24ac427571db4dd2924e9d4e7b0ca
Brinda M. Budhabhatti <78659448+BrindaBudhabhatti@users.noreply.github.com>
2025-10-14 02:39:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
+
+@st.cache_resource(show_spinner=False)
+def load_chain():
+    loader = TextLoader("cyber_data.txt")
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+    splits = splitter.split_documents(docs)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore = FAISS.from_documents(splits, embeddings)
+    llm = ChatGroq(model="llama-3.3-70b-versatile", api_key="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") 
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return chain
+
+qa_chain = load_chain()
+
+
+st.markdown("""
+<div class="header">
+    <h2 style='text-align:center;'>🛡️ CyberGuard AI</h2>
+    <p style='text-align:center;'>Your AI-powered Cyber Crime Awareness Assistant by <b>Brinda Budhabhatti</b></p>
+    <hr style="border-color: #30363D;">
+</div>
+""", unsafe_allow_html=True)
+
+
+## --------------------
+
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+    with st.chat_message("assistant"):
+            st.markdown("**Welcome to the CyberCrime Support Bot!**  "
+            "***I'm here to assist with cybercrime-related inquiries.***")
+
+# Display chat history
+for role, msg in st.session_state.chat_history:
+    if role == "You":
+        with st.chat_message("user"):
+            st.markdown(msg)
+    else:
+        with st.chat_message("assistant"):
+            st.markdown(msg)
+
+# with st.container(vertical_alignment='bottom'):
+# Input box
+user_query = st.chat_input("Type your question about cyber safety...")
+
+if user_query:
+    st.session_state.chat_history.append(("You", user_query))
+    with st.spinner("Analyzing your question..."):
+        result = qa_chain.invoke({"question": user_query})
+        answer = result["answer"]
+    st.session_state.chat_history.append(("CyberGuard AI", answer))
+    st.rerun()
